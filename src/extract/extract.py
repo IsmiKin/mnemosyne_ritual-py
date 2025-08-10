0fc2750e94d36a74978517a67e289561accb7472
Ismael Ors <ismikin@gmail.com>
2025-08-11 00:54:05 +0200

    chore(all): Add missing method descriptors - 2

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -5,10 +5,22 @@ import extract.constants.config as config
 
 
 def file_exists(file_path: str) -> bool:
+    """Check if the file exists at the given path.
+    Args:
+        file_path (str): The path to the file.
+    Returns:
+        bool: True if the file exists, False otherwise."""
     return os.path.isfile(file_path)
 
 
 def csv_to_dataframe(file_path: str) -> pd.DataFrame:
+    """Read a CSV file and return a DataFrame.
+    Args:
+        file_path (str): The path to the CSV file.
+    Returns:
+        pd.DataFrame: The DataFrame containing the CSV data.
+    Raises:
+        Exception: If there is an error reading the CSV file."""
     try:
         return pd.read_csv(file_path, sep=config.SEPARATOR, encoding=config.ENCODING)
     except Exception as error:
